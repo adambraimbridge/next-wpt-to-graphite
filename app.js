3d3200f1f886bec8b8c8edf75209ac2d018d6015
Harry Spriggs <harry.spriggs@ft.com>
2015-02-10 15:09:18 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+var WebPageTest = require('webpagetest');
+var http = require('http');
+var fs = require('fs');
+var net = require('net');
+
+var wpt = new WebPageTest();
+var test_ID;
+var test_status;
+var apiKey = process.env.HOSTEDGRAPHITE_APIKEY;
+var wpt_server = process.env.WPT_LOCATION;
+var location = process.argv[2];
+var pageType = process.argv[3];
+var url;
+var browser;
+var country;
+
+// Set url based on command line parameters (defaults to article page)
+if(pageType == 'article'){
+	url = 'next.ft.com/380e7966-b07f-11e4-9b8e-00144feab7de';
+}else if(pageType == 'homepage'){
+	url = 'next.ft.com'
+}else if(typeof pageType == 'undefined'){
+	url = 'next.ft.com/380e7966-b07f-11e4-9b8e-00144feab7de';
+	pageType = 'article';
+}
+console.log("URL: " + url);
+console.log("PageType: " + pageType);
+
+// Set a base location if one isn't provided
+if(typeof location == 'undefined'){
+	console.log('Missing required browser location parameter.  Correct usage is "node app.js browser_location {pageType}""');
+	return 'error';
+}
+
+// get Country and Browser metrics from the location
+country = location.substring(0,location.indexOf('_'));
+browser = location.substring(location.lastIndexOf(':')+1,location.length);
+console.log("Country: " + country);
+console.log("Browser: " + browser);
+
+// Star the test. Once started, initiate the check-for-results-loop
+wpt.runTest(url, {
+		"server":wpt_server,
+		"location":location,
+	}, 	function (err, data) {
+			test_ID = data.data.testId;
+			console.log(getStatus(test_ID));
+	}
+
+);
+
+// Keep checking that the test has finished.  Once it has, post the metrics
+function getStatus(id){
+
+	wpt.getTestStatus(id, {
+		"server":wpt_server
+	}, function(err, status_data){
+
+		test_status = status_data.statusCode;
+		console.log("Status: " + test_status);
+
+		if(test_status==200){
+			postData(id);
+		}else{
+			setTimeout(function(){
+				getStatus(id);
+			}, 5000);
+		}
+
+	});
+
+}
+
+// Post metrics to HostedGraphite
+function postData(id){
+
+	wpt.getTestResults(id, {
+		"server":wpt_server
+	}, function(err, results){
+
+		var postPayload =
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'firstView.bytesIn ' + 	results.data.runs['1'].firstView.bytesIn + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'firstView.docTime ' + 	results.data.runs['1'].firstView.docTime + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'firstView.fullyLoaded ' + 	results.data.runs['1'].firstView.fullyLoaded + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'firstView.render ' + 	results.data.runs['1'].firstView.render + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'firstView.visualComplete ' + 	results.data.runs['1'].firstView.visualComplete + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'repeatView.bytesIn ' + 	results.data.runs['1'].repeatView.bytesIn + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'repeatView.docTime ' + 	results.data.runs['1'].repeatView.docTime + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'repeatView.fullyLoaded ' + 	results.data.runs['1'].repeatView.fullyLoaded + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'repeatView.render ' + 	results.data.runs['1'].repeatView.render + '\n' +
+			apiKey + '.next.' + country + '.' + browser + '.' + pageType + '.' + 'repeatView.visualComplete ' + 	results.data.runs['1'].repeatView.visualComplete;
+
+		console.log('Payload: \n' + postPayload);
+
+
+var socket = net.createConnection(2003, "carbon.hostedgraphite.com", function(err) {
+			console.log("Connected to graphite");
+			socket.write(postPayload);
+			socket.end();
+		});
+
+	});
+
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+A simple program to check in-browser performance, using WebPageTest, against the next.ft.com website and report the most important metrics to a Hosted Graphite instance.
+
+**Usage:**
+>node app.js browser_location {pageType}
+
+**browser_location:** Where you want the browser to run.  Choices can be found at the WebPageTest instance's "locations" page: {host}/getLocations.php?f=xml.
+
+**pageType(optional):** Current choices are "article" or "homepage".  If no parameter is specified, it will default to "article".
+
+Make sure you have the following environment variables defined:
+ - WPT_LOCATION <- the hostname of your WebPageTest server
+ - HOSTEDGRAPHITE_APIKEY <- your Graphite API key
+
+Results are sent to a hosted graphite page https://www.hostedgraphite.com/app/
